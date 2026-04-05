c8112cab69ea353e946f77efd812704c5115d703
Levi Diamond <levi.diamond1@marist.edu>
2026-04-05 11:33:47 -0400

    Update lab7.sql
    Added SQL statements to update account balances and reset them.

--- lab 7/lab7.sql
+++ lab 7/lab7.sql
@@ -11,3 +11,46 @@ INSERT INTO Accounts (account_id, owner, balance) VALUES
 (2, 'Ben', 300),
 (3, 'Cara', 200);
 
+SELECT * 
+FROM Accounts
+WHERE account_id = 1
+UNION
+SELECT * 
+FROM Accounts
+WHERE account_id = 2;
+
+BEGIN;
+
+UPDATE Accounts
+SET balance = balance - 100
+WHERE account_id = 1 and balance >= 100;
+
+UPDATE Accounts
+SET balance = balance + 100
+WHERE account_id = 2;
+
+COMMIT;
+
+SELECT * 
+FROM Accounts
+WHERE account_id = 1
+UNION
+SELECT * 
+FROM Accounts
+WHERE account_id = 2;
+
+--resetting the balances to the original values
+BEGIN;
+
+UPDATE Accounts
+SET balance = balance + 100
+WHERE account_id = 1 
+
+UPDATE Accounts
+SET balance = balance - 100
+WHERE account_id = 2;
+
+COMMIT;
+
+
+
