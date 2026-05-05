cd60ed63b687c5fd46f43e9084a2f82e530417d9
Levi Diamond <levi.diamond1@marist.edu>
2026-05-04 20:00:25 -0400

    Update project.sql

--- project/project.sql
+++ project/project.sql
@@ -37,4 +37,70 @@ FOREIGN KEY (passer_id) REFERENCES Quarterbacks(passer_id)
 -- import the csvs from R to SQL
 \copy QBplays FROM '/Users/levidiamond/Desktop/Cmpt 308/project/QBplays.csv' CSV HEADER NULL 'NA';
 
+-- creating summation tables
+CREATE TABLE Game_Stats AS
+SELECT game_id, passer_id, 
+  SUM(passing_yards) AS game_pYards, 
+  SUM(pass_attempt) AS game_p_ATT, 
+  SUM(pass_touchdown) AS game_p_TD, 
+  SUM(complete_pass) AS game_p_Comp, 
+  SUM(qb_epa) AS game_qb_epa, 
+  SUM(shotgun) AS game_shotgun, 
+  SUM(qb_dropback) AS game_qbdropbacks, 
+  SUM(qb_scramble) AS game_qb_scrambles, 
+  SUM(yards_gained) AS game_total_yards
+FROM QBplays
+GROUP BY game_id, passer_id;
 
+CREATE TABLE Season_Stats AS 
+SELECT passer_id,
+  SUM(passing_yards) AS p_Yards, 
+  SUM(pass_attempt) AS p_ATT, 
+  SUM(pass_touchdown) AS p_TD, 
+  SUM(complete_pass) AS p_Comp, 
+  SUM(qb_epa) AS qb_epa, 
+  SUM(shotgun) AS shotgun, 
+  SUM(qb_dropback) AS qbdropbacks, 
+  SUM(qb_scramble) AS qb_scrambles, 
+  SUM(yards_gained) AS total_yards
+FROM QBplays
+GROUP BY passer_id;
+
+-- creating a procedure that adds plays to the play-by-play data table
+-- has to check if the QB or game is real
+-- has to check if that play already happened
+
+CREATE PROCEDURE add_play(p_play_id INT, p_game_id INT , p_passer_id INT, p_posteam INT, 
+  p_passing_yards INT, p_pass_attempt INT, p_pass_touchdown INT, p_complete_pass INT, 
+  p_qb_epa INT, p_shotgun INT, p_qb_dropback INT, p_qb_scramble INT , p_yards_gained INT)
+LANGUAGE plpgsql AS $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM Quarterbacks WHERE passer_id = p_passer_id) THEN
+    RAISE EXCEPTION 'That Quarterback does not exist', p_passer_id;
+  END IF;
+
+  IF NOT EXISTS (SELECT 1 FROM QBgames WHERE game_id = p_game_id) THEN
+    RAISE EXCEPTION 'That game has not happened', p_game_id;
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM QBplays 
+    WHERE play_id = p_play_id AND game_id = p_game_id
+    ) THEN
+       RAISE EXCEPTION 'This play already happened.', p_play_id, p_game_id;
+    END IF;
+
+  INSERT INTO QBplays(play_id, game_id, passer_id, posteam, 
+  passing_yards, pass_attempt, pass_touchdown, 
+  complete_pass, qb_epa, shotgun, qb_dropback, qb_scramble, yards_gained);
+
+  VALUES (
+    p_play_id, p_game_id, p_passer_id, p_posteam,
+    p_passing_yards, p_pass_attempt, p_pass_touchdown,
+    p_complete_pass, p_qb_epa, p_shotgun,
+    p_qb_dropback, p_qb_scramble, p_yards_gained
+  );
+
+END; $$;
+
+  
